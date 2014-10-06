ec25c63b53d63b2dd8fd3a6af1130b7e6c761236
paytonGOD <juanlp01@ucm.es>
2014-10-06 12:05:37 +0200

    Adding Splash Activity and 2 buttons , now going for the register and login screens

---
+++ app/src/main/java/com/bsod/tfg/vista/ActivitySplash.java
@@ -0,0 +1,42 @@
+package com.bsod.tfg.vista;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.bsod.tfg.R;
+
+public class ActivitySplash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Para realizar una pantalla tipo 'splash' deseamos ocultar la barra superior
+
+        //ActionBar actionBar = getActionBar();
+        //actionBar.hide();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_splash, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
